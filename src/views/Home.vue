0605315449a7d0c73ea4fd4a48f606944de8093a
Juan <jtripodi@alumnos.exa.unicen.edu.ar>
2021-11-06 19:51:10 -0300

    Placeholder de Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,9 +4,22 @@
       <span>INICIO</span>
     </h2>
     <hr />
-      <div class="text-center">
-        <img alt="Vue logo" src="../assets/logo.png" />
+    <div class="pa-2">
+    </div>
+    <div class="text-center mx-2">
+      <h3 class="black--text">
+      <span>MEVN STACK</span>
+      </h3>
+      <img alt="MongoDB logo" src="../assets/mongodb.png" :width="100"/>
+      <img alt="Express logo" src="../assets/express.png" :width="100"/>
+      <img alt="VueJS logo" src="../assets/vue.png" :width="100"/>
+      <img alt="NodeJS logo" src="../assets/node.png" :width="100"/>
+      <div class="pa-2">
       </div>
+      <h3 class="black--text">
+      <span>AUTH SERVICE</span>
+      </h3>
+      <img alt="Auth0 logo" src="../assets/auth0.png" :width="100"/>
     </div>
   </div>
 </template>
